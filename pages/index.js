6b10b73d364fc85417b320f81ad4e961777f97d0
Diogo de Souza <48259656+souzadediogo@users.noreply.github.com>
2023-09-12 21:01:46 +0000

    update pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Gatinha, eu te amo! Se tu também me ama, dá uma risadinha! </h1>
+    return <h1>Gatinha, eu te amo! Se tu também me ama, dá uma risadinha! ¬¬</h1>
 }
 
 export default Home;
