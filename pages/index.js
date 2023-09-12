337d899d8fa30830cbfa401e8b0cd9eb28747675
Diogo de Souza <48259656+souzadediogo@users.noreply.github.com>
2023-09-12 21:30:22 +0000

    Changed main message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Gatinha, eu te amo! Se tu também me ama, dá uma risadinha! ¬¬</h1>
+    return <h1>Gatinha, eu te amo! Se tu também me ama, dá uma risadinha! 😆</h1>
 }
 
 export default Home;
