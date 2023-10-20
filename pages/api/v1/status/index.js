72ad037d030dbacabf1b61849427eda720b467e0
Diogo de Souza <48259656+souzadediogo@users.noreply.github.com>
2023-10-20 13:30:17 +0000

    created  and connection to db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
   response
     .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+    .json({ chave: "Alunos do curso.dev são pessoas acima da média" });
 }
 
 export default status;
