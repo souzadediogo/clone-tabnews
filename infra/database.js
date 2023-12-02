18bddad004c615fb7d003629b1765039e7c19201
Diogo de Souza <diogodesouza@Diogos-MacBook-Pro.local>
2023-12-01 21:26:34 -0500

    started implementing status page api response

--- infra/database.js
+++ infra/database.js
@@ -6,11 +6,14 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD //local_password"
+    password: process.env.POSTGRES_PASSWORD, //local_password"
   });
   await client.connect();
 
   const result = await client.query(queryObject);
+  postgresVersion = () => {
+    return client.query("SELECT version()");
+  };
   await client.end();
 
   return result;
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,13 @@
 import database from "infra/database.js";
+import query from "psql/lib/query";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  // console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+  const postgres_version = query.postgres_version;
+  console.log(postgres_version);
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
